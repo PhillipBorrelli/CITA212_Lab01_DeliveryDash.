3d5379294e2d18830f881b1d404eb126a24e2b65
PhillipBorrelli <borrel281@morrisville.edu>
2026-09-14 23:42:24 -0400

    Using OnTriggerEnter2D

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -6,4 +6,8 @@ public class Collision : MonoBehaviour
     {
         Debug.Log("Ouch!");
     }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("What the heck was that?");
+    }
 }
