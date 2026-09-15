234fad82e3edc7644e5f42e7569f206d68e8d0d8
PhillipBorrelli <borrel281@morrisville.edu>
2026-09-14 20:46:45 -0400

    Introducing Methods

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,7 +5,7 @@ public class Driver : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.Rotate(0, 0, 45); 
     }
 
     // Update is called once per frame
